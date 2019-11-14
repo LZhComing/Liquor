53aedc17a3ebe11522278d002d5c03a041193d74
zhengzhicong <zhengzhicong@gridsum.com>
2019-11-14 10:37:40 +0800

    栈的压入，弹出序列

--- liquor-JianZhiOffer/src/main/java/com/liquorcloud/liquor/offer/no20to30/No23.java
+++ liquor-JianZhiOffer/src/main/java/com/liquorcloud/liquor/offer/no20to30/No23.java
@@ -8,6 +8,8 @@ import java.util.Stack;
  * 假设压入栈的所有数字均不相等。例如序列1,2,3,4,5是某栈的压入顺序，序列4,5,3,2,1是该压栈序列对应的一个弹出序列，但4,3,5,1,2就不可能是该压栈序列的弹出序列。
  * （注意：这两个序列的长度是相等的）
  *
+ * 思路：用一个栈来打辅助，定义一个弹出索引从出栈顺序的第一个开始，先进行入栈操作，当栈顶元素和弹出索引对应的元素相等，开始出栈，
+ * 同时弹出索引向右移动，如果入栈出栈操作完成，辅助栈不为空，证明不是弹出顺序
  *
  * @author zzc
  */
@@ -15,31 +17,17 @@ public class No23 {
 
     public boolean IsPopOrder(int [] pushA,int [] popA) {
 
-       /* Stack<Integer> stack = new Stack<>();
-        //先按入栈顺序入栈
-        for (int value : pushA) {
-            stack.push(value);
-        }
-        //再按出栈顺序出栈
-        for (int value : popA) {
-            if (stack.peek() != value) {
-                return false;
-            }
-            stack.pop();
-        }
-
-        return true;*/
-
        Stack<Integer> stack = new Stack<>();
        if(pushA.length!=popA.length){return false;}
+       //弹出索引
+       int popIndex = 0;
        for (int i=0;i<popA.length;i++){
-           while (i<pushA.length){
-               if(stack.peek()!=popA[i+1]){
-                   return false;
-               }
-
-           }
+            stack.push(pushA[i]);
+            while (!stack.isEmpty() && stack.peek() == popA[popIndex]){
+                stack.pop();
+                popIndex++;
+            }
        }
-       return true;
+       return stack.isEmpty();
     }
 }
