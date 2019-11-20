6a65adc5efbb4caefdc0e5b1ee4c7db6e2e6d038
zhengzhicong <zhengzhicong@gridsum.com>
2019-11-20 18:18:43 +0800

    Merge branch 'master' of E:\liquor with conflicts.

--- liquor-JianZhiOffer/src/main/java/com/liquorcloud/liquor/offer/no20to30/No25.java
+++ liquor-JianZhiOffer/src/main/java/com/liquorcloud/liquor/offer/no20to30/No25.java
@@ -1,46 +1,57 @@
 package com.liquorcloud.liquor.offer.no20to30;
 
-import java.util.ArrayList;
-
 /**
- * 二叉树中和为某一值的路径
- * 输入一颗二叉树的跟节点和一个整数，打印出二叉树中结点值的和为输入整数的所有路径。
- * 路径定义为从树的根结点开始往下一直到叶结点所经过的结点形成一条路径。
- * (注意: 在返回值的list中，数组长度大的数组靠前)
+ * 输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历的结果。如果是则输出Yes,否则输出No。假设输入的数组的任意两个数字都互不相同。
  *
- *
- *
- *
+ * 看到这个题目第一感觉这是啥玩意，但是不能就此放弃
+ * 仔细分析题目，找到两个要点，分别为：“二叉搜索树”、“后序遍历”
+ * 从而可以确定两个特性，1：输入数组的最后一个数字为二叉树的根、2：如果去掉最后一个数字，剩下的数字可以分为left/right两部分，且left全部比right小
+ * 且left和right都是符合规律的后序遍历顺序
+ * 因此，要用递归地判断，如果不满足，返回false
  * @author zzc
  */
 public class No25 {
 
-    class TreeNode {
-        int val = 0;
-        TreeNode left = null;
-        TreeNode right = null;
-
-        public TreeNode(int val) {
-            this.val = val;
+    public boolean VerifySquenceOfBST(int [] sequence) {
+        if (sequence.length==0){
+            return false;
         }
+        return checkTree(sequence,0,sequence.length-1);
     }
 
-    public ArrayList<ArrayList<Integer>> FindPath(TreeNode root, int target) {
-        ArrayList<ArrayList<Integer>>  result = new ArrayList<>();
-
-        if(root==null){return result;}
-
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(root.val);
-        target -= root.val;
-        if (target==0 && root.left==null && root.right==null){
-            result.add(list);
+    private boolean checkTree(int[] sequence,int start,int end){
+        if(start>=end){
+            //开始和结束重合，说明遍历完了
+            return true;
         }
-        FindPath(root.left,target);
-        FindPath(root.right,target);
-        list.remove(list.size()-1);
+        int i = 0;
+        for (;i<end-1;i++){
+            //找到左子树和右字数的分界线
+            if(sequence[i]>sequence[end]){
+                break;
+            }
+        }
+        for(;i<end-1;i++){
+            //判断右子树是否都满足规则
+            if(sequence[i]<sequence[end]){
+                return false;
+            }
+        }
 
-        return result;
+        boolean left = true;
+        //当左子树不为空时
+        if (i>start){
+            left = checkTree(sequence,start,i-1);
+        }
+        boolean right = true;
+        //当右子树不为空时
+        if (i<end){
+            right = checkTree(sequence,i,end-1);
+        }
 
+
+        return left && right;
     }
+
+
 }
---
+++ liquor-JianZhiOffer/src/main/java/com/liquorcloud/liquor/offer/no20to30/No26.java
@@ -0,0 +1,4 @@
+package com.liquorcloud.liquor.offer.no20to30;
+
+public class No26 {
+}
