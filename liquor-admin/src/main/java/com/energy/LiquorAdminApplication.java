769f24a3016a53eb27b515a0048227e6aba6c226
zhengzhicong <zhengzhicong@gridsum.com>
2020-10-15 14:01:22 +0800

    增加spring boot admin监控程序

---
+++ liquor-admin/src/main/java/com/energy/LiquorAdminApplication.java
@@ -0,0 +1,59 @@
+package com.energy;
+
+
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+/**
+ * spring boot admin监控服务器
+ * @author zzc on 2020.10.15
+ */
+@Configuration
+@EnableAutoConfiguration
+@EnableDiscoveryClient
+@EnableAdminServer
+public class LiquorAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquorAdminApplication.class, args);
+	}
+
+	@Configuration
+	public static class SecuritySecureConfig extends WebSecurityConfigurerAdapter {
+		private final String adminContextPath;
+
+		public SecuritySecureConfig(AdminServerProperties adminServerProperties) {
+			this.adminContextPath = adminServerProperties.getContextPath();
+		}
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+			successHandler.setTargetUrlParameter("redirectTo");
+			successHandler.setDefaultTargetUrl(adminContextPath + "/");
+
+            http.authorizeRequests()
+                    .antMatchers(adminContextPath + "/assets/**").permitAll()
+                    .antMatchers(adminContextPath + "/login").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                    .formLogin()
+                    .loginPage(adminContextPath + "/login")
+                    .successHandler(successHandler).and()
+                    .logout()
+                    .logoutUrl(adminContextPath + "/logout").and()
+                    .httpBasic().and()
+                    .csrf()
+                    .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                    .ignoringAntMatchers(adminContextPath + "/instances", adminContextPath + "/actuator/**");
+        }
+    }
+}
